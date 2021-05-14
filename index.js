694240ad8743e9477332e6018983996066290b50
jean <jin.ahn.1027@gmail.com>
2021-05-14 13:00:58 -0400

    commit tree 1fc034ebc1b81c1f850904392fc3433a01b05df0

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    let words=str.split(" ");
+    for(let i=0; i<words.length; i++){
+      words[i]=words[i].charAt(0).toUpperCase() + words[i].slice(1)
+    }
+    return words.join(" ");
+  }
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(str){
+    const exceptarr=['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words=str.split(" ");
+    words[0]=this.capitalize(words[0]);
+    
+    for(let i=1; i<words.length; i++){
+      if (!(exceptarr.includes(words[i]))){
+        words[i]=this.capitalize(words[i]);
+      }
+    }
+    return words.join(" ");
+  }  
 }
